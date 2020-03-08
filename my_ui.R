c4c7a85600599b9055540937eabaa96cac90db8d
Samyak Jain <samyak26@uw.edu>
2020-03-07 17:59:26 -0800

    Adding raw files

--- my_ui.R
+++ my_ui.R
@@ -1,6 +1,9 @@
 install.packages("shiny")
 library("shiny")
 
+bitcoin <-  read.csv("Binance_BTCUSDT_d.csv", stringsAsFactors = FALSE)
+ethereum <- read.csv("Binance_ETHUSDT_d.csv", stringsAsFactors = FALSE)
+
 Main_UI_Section <- navbarPage(
   "Trends in Cryptocurrencies!",
   Home,
