59572881b9a098ad9b387769c23c54780c87bf73
Samyak Jain <samyak26@uw.edu>
2020-03-09 00:52:26 -0700

    Reconfig of functions

--- my_ui.R
+++ my_ui.R
@@ -3,14 +3,6 @@ library("shiny")
 bitcoin <-  read.csv("Binance_BTCUSDT_d.csv", stringsAsFactors = FALSE)
 ethereum <- read.csv("Binance_ETHUSDT_d.csv", stringsAsFactors = FALSE)
 
-my_ui <- navbarPage(
-  "Trends in Cryptocurrencies!",
-  Home,
-  Question1,
-  Question2,
-  Question3
-)
-
 Home <- tabPanel(
   "Label_Home"
 )
@@ -25,4 +17,12 @@ Question2 <- tabPanel(
 
 Question3 <- tabPanel(
   "Label_Question3"
+)
+
+my_ui <- navbarPage(
+  "Trends in Cryptocurrencies!",
+  Home,
+  Question1,
+  Question2,
+  Question3
 )
