005c4afeaf25bfeedfcfd3f1e03265643242b1e4
Samyak Jain <samyak26@uw.edu>
2020-03-09 00:40:45 -0700

    Debug

--- my_ui.R
+++ my_ui.R
@@ -1,10 +1,9 @@
-install.packages("shiny")
 library("shiny")
 
 bitcoin <-  read.csv("Binance_BTCUSDT_d.csv", stringsAsFactors = FALSE)
 ethereum <- read.csv("Binance_ETHUSDT_d.csv", stringsAsFactors = FALSE)
 
-Main_UI_Section <- navbarPage(
+my_ui <- navbarPage(
   "Trends in Cryptocurrencies!",
   Home,
   Question1,
